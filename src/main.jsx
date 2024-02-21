cc9ca3d5ea3815d329855bde80b7f1ba011683e2
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-02-21 14:37:13 -0600

    Layout for Home Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage.jsx"
+import Layout from "./pages/Layout.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App msg={"Hello, from router!"}/>,
+    element: <Layout />,
     errorElement: <ErrorPage />,
   },
 ]);
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,21 @@
+import Header from "../components/Header";
+import Home from "../components/Home";
+import Footer from "../components/Footer";
+
+
+/**
+ * Returns the layout for home page.
+ * 
+ * @return Layout for page
+ */
+function Layout() {
+    return (
+        <div>
+            <Header />
+            <Home />
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;
