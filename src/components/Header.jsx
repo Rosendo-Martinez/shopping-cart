fe6c8ee96efc9d771d0e056d8a91331b0c20d41e
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-02-21 18:53:14 -0600

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+import star from "../assets/star.png";
+import cart from "../assets/cart.png";
+import classes from "../styles/header.module.css";
+
 function Header() {
-  return <p>Header!</p>;
+  return (
+    <div className={classes.header}>
+      <div className={classes.left}>
+        <p className={classes.headerTitle}>Posters, Quotes and Gulag</p>
+        <img className={classes.star} src={star} />
+      </div>
+
+      <div className={classes.center}>
+        <Link className={classes.link} to={"/"}>
+          Home
+        </Link>
+        <Link className={classes.link} to={"/Shop"}>
+          Shop
+        </Link>
+      </div>
+
+      <div className={classes.right}>
+        <button className={classes.cartButton}>
+          <img className={classes.cart} src={cart} />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
