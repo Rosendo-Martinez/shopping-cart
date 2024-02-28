68f9488639b6acd4430f0f6297b074eb542a233b
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-02-27 19:48:12 -0600

    Shop: page selector refactor

--- src/components/PageSelector.jsx
+++ src/components/PageSelector.jsx
@@ -1,22 +1,19 @@
-import { getShopPageURLS } from "../functions/functions";
 import { Link } from "react-router-dom";
 
-export default function PageSelector() {
-  const shopPageURLS = getShopPageURLS();
-  const pageURLIndex = 0;
-
+export default function PageSelector({ shopPageURLS, activePageURLIndex }) {
   return (
     <div>
-      {pageURLIndex != 0 ? (
-        <Link to={shopPageURLS[pageURLIndex - 1]}>Back</Link>
+      <div>Current page: {activePageURLIndex + 1}</div>
+      {activePageURLIndex != 0 ? (
+        <Link to={shopPageURLS[activePageURLIndex - 1]}>Back</Link>
       ) : null}
       {shopPageURLS.map((url, i) => (
         <Link key={url} to={url}>
           {i + 1}
         </Link>
       ))}
-      {pageURLIndex < shopPageURLS.length - 1 ? (
-        <Link to={shopPageURLS[pageURLIndex + 1]}>Next</Link>
+      {activePageURLIndex < shopPageURLS.length - 1 ? (
+        <Link to={shopPageURLS[activePageURLIndex + 1]}>Next</Link>
       ) : null}
     </div>
   );
--- src/components/ShopCatalog.jsx
+++ src/components/ShopCatalog.jsx
@@ -1,23 +1,42 @@
 import SortBy from "./SortBy";
 import CardsContainer from "./CardsContainer";
 import { getShopData } from "../functions/functions";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import PageSelector from "./PageSelector";
 
 function ShopCatalog() {
   const shopItems = useLoaderData();
+  const { pageNumber } = useParams();
 
   return (
     <div>
       <SortBy />
       <CardsContainer shopItems={shopItems} />
-      <PageSelector />
+      <PageSelector
+        shopPageURLS={getShopPageURLS(5, "?col=12")}
+        activePageURLIndex={
+          pageNumber === undefined ? 0 : Number(pageNumber) - 1
+        }
+      />
     </div>
   );
 }
 
 export async function loadShopItems() {
   return getShopData();
+}
+
+/**
+ * Creates and returns the links to the shop pages with the given queries.
+ *
+ * @param {number} numberOfPages
+ * @param {string} queries example: ?color=red&width=12
+ * @returns
+ */
+function getShopPageURLS(numberOfPages, queries = "") {
+  return Array(numberOfPages)
+    .fill(null)
+    .map((v, i) => `/shop/page/${1 + i}${queries}`);
 }
 
 export default ShopCatalog;
