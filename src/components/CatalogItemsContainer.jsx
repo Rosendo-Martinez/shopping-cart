c16289a887a5d82a60beed19ceef61f212e60155
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-03-01 15:56:12 -0600

    Style CatalogItemsContainer

--- src/components/CatalogItemsContainer.jsx
+++ src/components/CatalogItemsContainer.jsx
@@ -1,19 +1,22 @@
 import CatalogItem from "./CatalogItem";
 import { ITEM_LINK_PREFIX } from "../constants";
 import PropTypes from "prop-types";
+import classes from "../styles/catalogItemsContainer.module.css";
 
 function CatalogItemsContainer({ shopItems }) {
   return (
-    <div>
-      {shopItems.map((cd) => (
-        <CatalogItem
-          key={cd.id}
-          image={cd.image}
-          title={cd.title}
-          cost={cd.cost}
-          itemStorePage={`${ITEM_LINK_PREFIX}/${cd.id}`}
-        />
-      ))}
+    <div className={classes.container}>
+      <div className={classes.grid}>
+        {shopItems.map((cd) => (
+          <CatalogItem
+            key={cd.id}
+            image={cd.image}
+            title={cd.title}
+            cost={cd.cost}
+            itemStorePage={`${ITEM_LINK_PREFIX}/${cd.id}`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
