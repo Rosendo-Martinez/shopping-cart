6a859e78b02acbebe692805d69742ca07dd7a9b8
Rosendo Martinez <rosendomartinez270@gmail.com>
2024-03-05 15:43:09 -0600

    Move Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import PropTypes from "prop-types";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import Layout from "./pages/Layout.jsx";
+import Shop from "./pages/Shop.jsx";
+import Home from "./pages/Home.jsx";
+import ShopCatalog, { loadShopItems } from "./components/ShopCatalog.jsx";
+import ShopItemPage, { loadShopItem } from "./components/ShopItemPage.jsx";
+import Cart, { loadCart } from "./components/Cart.jsx";
 
-function App({ msg }) {
-  return <h1>{msg}</h1>;
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "shop",
+          element: <Shop />,
+          children: [
+            { index: true, element: <ShopCatalog />, loader: loadShopItems },
+            {
+              path: "page/:pageNumber",
+              element: <ShopCatalog />,
+              loader: loadShopItems,
+            },
+            {
+              path: "item/:itemID",
+              element: <ShopItemPage />,
+              loader: loadShopItem,
+            },
+            {
+              path: "cart",
+              element: <Cart />,
+              loader: loadCart,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
-
-App.propTypes = {
-  msg: PropTypes.string,
-};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,49 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./pages/ErrorPage.jsx";
-import Layout from "./pages/Layout.jsx";
-import Shop from "./pages/Shop.jsx";
-import Home from "./pages/Home.jsx";
-import ShopCatalog, { loadShopItems } from "./components/ShopCatalog.jsx";
-import ShopItemPage, { loadShopItem } from "./components/ShopItemPage.jsx";
-import Cart, { loadCart } from "./components/Cart.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <Home /> },
-      {
-        path: "shop",
-        element: <Shop />,
-        children: [
-          { index: true, element: <ShopCatalog />, loader: loadShopItems },
-          {
-            path: "page/:pageNumber",
-            element: <ShopCatalog />,
-            loader: loadShopItems,
-          },
-          {
-            path: "item/:itemID",
-            element: <ShopItemPage />,
-            loader: loadShopItem,
-          },
-          {
-            path: "cart",
-            element: <Cart />,
-            loader: loadCart,
-          },
-        ],
-      },
-    ],
-  },
-]);
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
